c13ccf3d80d1a33c56cb7e2d4e2882ea52613f31
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-07-23 16:22:52 +0300

    feat: добавить корректный скрипт для перезаписи и закрытия попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,37 @@
 const popupButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__input");
+// Находим поля формы в DOM
+let nameInput = document.querySelector(".popup__input-text_type_name");
+let jobInput = document.querySelector(".popup__input-text_type_work");
+
+console.log(formElement, nameInput, jobInput);
 
 function togglePopup() {
     console.log("click", popup);
    popup.classList.toggle("popup_is-opened");
 }
 
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы, так мы можем определить свою логику отправки.
+    // значение полей jobInput и nameInput из свойства value
+    let ValNameInput = nameInput.value;
+    let ValJobInput = jobInput.value;
+    console.log(nameInput.value, jobInput.value);
+     // Выбериаем элементы, куда должны быть вставлены значения полей
+    let profileNameInput = document.querySelector(".profile__title");
+    let profileJobInput = document.querySelector(".profile__text");
+    console.log(profileNameInput, profileJobInput);
+    // Вставим новые значения с помощью textContent
+    profileNameInput.textContent = ValNameInput;
+    profileJobInput.textContent =  ValJobInput;
+}
+
 popupButton.addEventListener("click", togglePopup);
+// Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", formSubmitHandler, togglePopup); 
+formElement.addEventListener("submit", togglePopup); 
 closeButton.addEventListener("click", togglePopup);

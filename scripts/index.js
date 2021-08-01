ea55b0864df82dafd7f110944dfb643bbf0c2a1a
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-08-01 19:33:25 +0300

    feat: добавить попап для кнопки add

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,60 @@
-const popupButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".popup__close");
-const popup = document.querySelector(".popup");
+const popupButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+const popupAdd = document.getElementById('popup-add');
+const closeButtonAdd = document.getElementById('closeButtonAdd');
+
+console.log(popupButton, closeButton, popup, popupAdd);
 // Находим форму в DOM
-let formElement = document.querySelector(".popup__input");
+let formElement = document.querySelector('.popup__input');
 // Находим поля формы в DOM
-let nameInput = document.querySelector(".popup__input-text_type_name");
-let jobInput = document.querySelector(".popup__input-text_type_work");
+let nameInput = document.querySelector('.popup__input-text_type_name');
+let jobInput = document.querySelector('.popup__input-text_type_work');
+// Находим template-элемент и сам элемент в html
+const ElementTemplate = document.getElementById('element-template');
+const Element = document.querySelector('.element');
+// Находим кнопку для вызова создания template-элемент
+const AddButton = document.querySelector('.profile__add-button');
+// Находим поля заполнения карточки: картинка, заголовок
+let ElementName = document.querySelector('.element__title');
+let ElementImg = document.querySelector('.element__image');
 
-console.log(formElement, nameInput, jobInput);
+const initialElement =  [ {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
 
+// функция  открытия - закрытия попапа для кнопки edit
 function togglePopup() {
     console.log("click", popup);
-   popup.classList.toggle("popup_is-opened");
+    popup.classList.toggle("popup_is-opened");
+}
+
+// функция  открытия - закрытия попапа для кнопки add
+function togglePopupAdd() {
+    console.log("click", popupAdd);
+    popupAdd.classList.toggle("popup_is-opened");
 }
 
 // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
@@ -28,11 +71,31 @@ function formSubmitHandler (evt) {
     // Вставим новые значения с помощью textContent
     profileNameInput.textContent = ValNameInput;
     profileJobInput.textContent =  ValJobInput;
-    togglePopup()
-    /*popup.classList.toggle("popup_is-opened");*/
+    togglePopup(popup);
 }
 
+// функция инициализации элементов/карточек
+/*function initElement (card) {
+    
+    const NewElement = ElementTemplate.contentEditable.firstElementChild.cloneNode(true);
+    NewElement.querySelector('.element__title').innerText = ElementName.value;
+
+    Element.appendChild(NewElement);
+} */
+
 popupButton.addEventListener("click", togglePopup);
+AddButton.addEventListener('click',togglePopupAdd);
 // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 formElement.addEventListener("submit", formSubmitHandler); 
 closeButton.addEventListener("click", togglePopup);
+closeButtonAdd.addEventListener("click", togglePopupAdd);
+
+// функция создания нового элемента/карточки
+/*AddButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    initElement();
+    nameInput.value = '';
+
+}); 
+
+initialElement.forEach(initElement); */

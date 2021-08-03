e2b87ac13bc95f5872702a3b45bc1acd61d39b64
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-08-03 11:17:31 +0300

    fix: вынести данные для карточек в отдельный файл initial-cards.js

--- scripts/index.js
+++ scripts/index.js
@@ -23,33 +23,6 @@ const element = document.querySelector('.elements');
 const addButton = document.querySelector('.profile__add-button');
 
 
-const initialElement =  [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 // функция  открытия - закрытия попапа для кнопки edit
 function togglePopup() {
     console.log('click', popup);

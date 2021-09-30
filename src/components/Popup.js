2b64f70d5793a7dddf5a9423f41b612baf049bd0
Daria Kamenskaya <daria.kamenskaya@yandex.ru>
2021-09-30 22:28:32 +0300

    refactor: сделать element приватным в классе Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,23 +5,23 @@ export class Popup {
    _openedClass = 'popup_is-opened';
 
   constructor(className) {
-    this.element = document.querySelector(className);
-    this.closeBtn = this.element.querySelector('.popup__close');
-    this.cardName = this.element.querySelector('.popup__title');
-    this.cardImg = this.element.querySelector('.popup__image');
+    this._element = document.querySelector(className);
+    this.closeBtn = this._element.querySelector('.popup__close');
+    this.cardName = this._element.querySelector('.popup__title');
+    this.cardImg = this._element.querySelector('.popup__image');
   }
 
 
   // функция открытия попапа
   openPopup() {
-      this.element.classList.add(this._openedClass);
+      this._element.classList.add(this._openedClass);
       // навешиваем слушатели
       this.setEventListeners();
     }
     
   // функция закрытия попапа
   closePopup() {
-      this.element.classList.remove(this._openedClass);
+      this._element.classList.remove(this._openedClass);
       // удаляем слушатели
       this.removeEventListeners();
     }
@@ -35,7 +35,7 @@ export class Popup {
 
   // функция закрытия по esc
   _handleEscClose = (evt) => {
-      if (evt.key === 'Escape' && this.element.classList.contains(this._openedClass)) {
+      if (evt.key === 'Escape' && this._element.classList.contains(this._openedClass)) {
           this.closePopup();
       }
   }
@@ -46,7 +46,7 @@ export class Popup {
     // закрытие по кнопке крестик
     this.closeBtn.addEventListener('click', () => this.closePopup());
     // закрытие по клику на оверлей
-    this.element.addEventListener("click", this._closePopupOverlay);
+    this._element.addEventListener("click", this._closePopupOverlay);
     // Закрытие попапа нажатием на Esc
     document.addEventListener('keydown', this._handleEscClose);
   }
@@ -57,7 +57,7 @@ export class Popup {
     // удаление слушателя закрытие по кнопке крестик
     this.closeBtn.removeEventListener('click', () => this.closePopup());
     // удаление слушателя закрытие по клику на оверлей
-    this.element.removeEventListener("click", this._closePopupOverlay);
+    this._element.removeEventListener("click", this._closePopupOverlay);
     // удаление слушателя закрытие попапа нажатием на Esc
     document.removeEventListener('keydown', this._handleEscClose);
   }
